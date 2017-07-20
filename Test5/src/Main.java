09f196e879df91f022fdbd5ef9f5428b2fa082a6
Nervviks <Nervvikss@gmail.com>
2017-07-20 15:31:21 +0300

    f

--- Test5/src/Main.java
+++ Test5/src/Main.java
@@ -3,11 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("master2112");
-=======
+
 		System.out.println("tes1t");
->>>>>>> refs/remotes/origin/Test
+
 	}
 
 }
